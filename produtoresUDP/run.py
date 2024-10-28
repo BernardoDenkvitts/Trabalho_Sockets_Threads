3358e930247054703474e98cca73525800ef4af8
bernardoArcari <BernardoArcari@gmail.com>
2024-10-28 07:26:03 -0300

    refactor: arquivos removidos

--- produtoresUDP/run.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-if __name__ == '__main__':
-    movie = subprocess.Popen(["python", "movie_producer.py"])
-    sports = subprocess.Popen(["python", "sport_producer.py"])
-    movie.wait()
-    sports.wait()
